e9ea2b3eae492083980a5ef5a8eb1296bf825576
David Lima <47418013+DavidProf@users.noreply.github.com>
2021-10-17 19:23:09 -0300

    feat(http server): add http module as example

---
+++ src/modules/http/server.ts
@@ -0,0 +1,14 @@
+import http from 'http'
+
+const bootstrapServer = () => {
+    const server = http.createServer()
+
+    server.on('request', (req, res) => {
+        res.setHeader('content-type', 'application/json')
+        res.end({ ok: true })
+    })
+    
+    server.on('listening', () => console.log('server running'))
+    
+    server.listen(3141)    
+}
