2f1c749809395ad20751813dd58785a610d2744b
David Lima - A.k.a. Xarlyu <47418013+DavidProf@users.noreply.github.com>
2021-11-09 20:05:21 -0300

    perf(logger): replace console with stdout and stderr
    done #15

--- src/commons/logger.ts
+++ src/commons/logger.ts
@@ -1,4 +1,4 @@
-import Console from 'console'
+import { stdout, stderr } from 'process'
 /**
  * boolean variable that inform if logs must show in the local environment
  */
@@ -24,7 +24,12 @@ export type LogMessage = string | Error | unknown
 /**
  * Log level names
  */
-export type LogLevelName = 'info' | 'warn' | 'error' | 'debug'
+export enum LogLevelName {
+    info = 'info',
+    warn = 'warn',
+    error = 'error',
+    debug = 'debug'
+}
 
 type LogArgs = { [key: string]: unknown }
 
@@ -33,6 +38,30 @@ const _logPrefix = {
     warn: '[\x1b[33mWARN\x1b[0m]',
     error: '[\x1b[31mERROR\x1b[0m]',
     debug: '[\x1b[95mDEBUG\x1b[0m]'
+}
+const _streamMap = {
+    [LogLevelName.info]: stdout,
+    [LogLevelName.debug]: stdout,
+    [LogLevelName.warn]: stderr,
+    [LogLevelName.error]: stderr
+}
+/**
+ * returns the standard output stream for the system
+ * @param {LogLevelName} level Log level name
+ * @returns {stdout|stderr} standard stream
+ */
+const _getStandardStream = (level: LogLevelName) => _streamMap[level]
+/**
+ * compose args in a object
+ * @param {LogArgs[]} args args to compose
+ * @returns {*|null} the object composed or null
+ */
+const _composeArgs = (args: LogArgs[]) => {
+    let composed: { [key: string]: unknown } | null = null
+    for (let index = 0; index < args.length; index++) {
+        composed = { ...(composed ?? {}), ...args[index] }
+    }
+    return composed
 }
 /**
  * Log anything following the rules in config
@@ -44,34 +73,21 @@ const _logPrefix = {
 const _log = (level: LogLevelName, message: LogMessage, ...args: LogArgs[]) => {
     if (!LOGGER_SHOW_LOCAL) return
 
-    Console[level](_logPrefix[level], message, ...args)
-}
-/**
- * Log anything following the rules in config as JSON
- * @param {LogLevelName} level Log level name
- * @param {LogMessage} message the principal message to log
- * @param {LogArgs} args complemental args
- * @returns {void}
- */
-const _logJSON = (
-    level: LogLevelName,
-    message: LogMessage,
-    ...args: LogArgs[]
-) => {
-    if (!LOGGER_SHOW_LOCAL) return
-
-    let composed = {}
-    for (let index = 0; index < args.length; index++) {
-        composed = { ...composed, ...args[index] }
+    if (LOGGER_OUTPUT_TYPE !== OUTPUT_TYPE.json) {
+        _getStandardStream(level).write(
+            `${_logPrefix[level]} ${message} ${
+                args.length ? JSON.stringify(_composeArgs(args)) : ''
+            }\n`
+        )
+    } else {
+        _getStandardStream(level).write(
+            `${JSON.stringify({
+                level: level,
+                message,
+                data: _composeArgs(args)
+            })}\n`
+        )
     }
-
-    Console[level](
-        JSON.stringify({
-            level: level,
-            message,
-            data: composed
-        })
-    )
 }
 /**
  * creates a log function
@@ -79,11 +95,8 @@ const _logJSON = (
  * @returns {Function} a log function
  */
 const createLogFunction = (level: LogLevelName) => {
-    return LOGGER_OUTPUT_TYPE !== OUTPUT_TYPE.json
-        ? (message: LogMessage, ...args: LogArgs[]) =>
-              _log(level, message, ...args)
-        : (message: LogMessage, ...args: LogArgs[]) =>
-              _logJSON(level, message, ...args)
+    return (message: LogMessage, ...args: LogArgs[]) =>
+        _log(level, message, ...args)
 }
 /**
  * logger helper
@@ -94,25 +107,25 @@ export const logger = {
      * @param message the principal message to log
      * @param args complemental args
      */
-    info: createLogFunction('info'),
+    info: createLogFunction(LogLevelName.info),
     /**
      * Log a warn
      * @param message the principal message to log
      * @param args complemental args
      */
-    warn: createLogFunction('warn'),
+    warn: createLogFunction(LogLevelName.warn),
     /**
      * Log an error
      * @param message the principal message to log
      * @param args complemental args
      */
-    error: createLogFunction('error'),
+    error: createLogFunction(LogLevelName.error),
     /**
      * Log for debug
      * @param message the principal message to log
      * @param args complemental args
      */
-    debug: createLogFunction('debug')
+    debug: createLogFunction(LogLevelName.debug)
 }
 
 export default logger
